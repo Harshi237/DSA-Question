5c6c34e0d0a3ee16dd674db82c47bec777f401f9
Harshita Gupta <166702000+Harshi237@users.noreply.github.com>
2026-07-01 23:49:36 +0530

    Time: 40 ms (37.77%), Space: 47.8 MB (31.61%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        dp[m][n] = true;
+        for (int i = 0; i < m; i++) {
+            dp[i][n] = false;
+        }
+        for (int j = n-1; j >= 0; j--) {
+            if (p.charAt(j) == '*')
+                dp[m][j] = dp[m][j +1];
+            else
+                dp[m][j] = false;
+        }
+        
+        for (int i = m-1; i >=0; i--) {
+            for (int j =n-1; j >=0; j--) {
+                char ch = p.charAt(j);
+                if (s.charAt(i) == ch || ch == '?')
+                    dp[i][j] = dp[i+1][j+1];
+
+                else if (ch == '*')
+                    dp[i][j] = dp[i+1][j] || dp[i][j+1];
+
+                else
+                    dp[i][j] = false;
+            }
+        }
+
+        return dp[0][0];
+    }
+}
